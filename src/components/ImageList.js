599034b031bc8e362240d1be39a910210e863b85
Hazari <79424896+Hazaricoolbudy@users.noreply.github.com>
2022-06-15 18:05:13 +0530

    final css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ImageList = (props) => {
    const images= props.images.map((image)=>{
         return (
-            <img key={image.id} src={image.urls.regular}/>
+            <img alt={image.description} key={image.id} src={image.urls.regular}/>
         )
     })
     
