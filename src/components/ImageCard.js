620a798988c52b5c46bc3e00f5a32745d8296585
Hazari <79424896+Hazaricoolbudy@users.noreply.github.com>
2022-06-29 18:24:43 +0530

    add crausel to the image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,31 +1,35 @@
 import React, { Component } from 'react'
 
 export default class ImageCard extends Component {
-constructor(props) {
-  super(props)
-  this.state={span:0}
+  constructor(props) {
+    super(props)
+    this.state = { span: 0 }
 
-  this.imageRef=React.createRef();
-}
-componentDidMount(){
-  this.imageRef.current.addEventListener('load',this.setSpans)
-}
-setSpans=()=>{
- const height=this.imageRef.current.clientHeight;
- const span=Math.ceil(height/10);
- this.setState({span})
+    this.imageRef = React.createRef();
+  }
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans)
+  }
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const span = Math.ceil(height / 10);
+    this.setState({ span })
 
-}
+  }
 
   render() {
     return (
-      <div style={{gridRowEnd:`span ${this.state.span}`}}>
-        <img 
-        ref={this.imageRef}
-        alt={this.props.image.description}
-        src={this.props.image.urls.regular}
-        />
+      <div>
+        <div style={{ gridRowEnd: `span ${this.state.span}` }}>
+          <img
+            ref={this.imageRef}
+            alt={this.props.image.description}
+            src={this.props.image.urls.regular}
+          />
+        </div>
+
       </div>
     )
   }
 }
+
